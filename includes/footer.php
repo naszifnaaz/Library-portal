69e0be22408dbee7bbef6bec79d4d76102f8be7d
Muhammed Nasif <73707865+naszifnaaz@users.noreply.github.com>
2020-11-18 19:18:20 +0530

    added headers and footers

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2020 Online Library Management System |<a href="https://github.com/naszifnaaz/" target="_blank"  > Designed by : MUHAMMED NASIF</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
